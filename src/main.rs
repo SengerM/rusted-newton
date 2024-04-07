6752e5f1f062c8d1b0136088cbadb3ea895e8f52
Matias Senger <m.senger@hotmail.com>
2024-04-07 19:49:09 +0200

    New version using `Vector3D` from `euclid` crate

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,13 @@
-use std::marker::PhantomData;
+use euclid;
 
-#[derive(Debug)]
-struct PhysicalVector<T> {
-    x: f64,
-    y: f64,
-    z: f64,
-    _t: PhantomData<T>,
-}
-
-impl<T> PhysicalVector<T> {
-    fn add(&mut self, other: &Self) {
-        self.x = self.x + other.x;
-        self.y = self.y + other.y;
-        self.z = self.z + other.z;
-    }
-    
-    fn new(x: f64, y: f64, z: f64) -> Self {
-        Self {
-            x,
-            y,
-            z,
-            _t: PhantomData,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Position;
-
-#[derive(Debug)]
-struct Velocity;
+enum Position {}
+enum Velocity {}
 
 fn main() {
-    let mut pos = PhysicalVector::<Position>::new(0., 0., 0.);
-    //~ let mut vel = PhysicalVector::<Velocity> {x:1., y:0., z:0., _t:PhantomData};
     
-    dbg!(&pos);
+    let pos = euclid::Vector3D::<f64, Position>::new(0.,2.,0.);
+    let pos2 = euclid::Vector3D::<f64, Velocity>::new(1.,1.,0.);
     
-    pos.add(&PhysicalVector::<Position>::new(0., 0., 1.));
-    pos.add(&PhysicalVector::<Velocity>::new(0., 0., 1.));
-    
-    dbg!(&pos);
+    println!("Result: {:?}", pos + pos2);
+
 }
