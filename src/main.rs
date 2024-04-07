17d939ba436188390ee86e3836cf4f93d9f81068
Matias Senger <m.senger@hotmail.com>
2024-04-07 19:18:33 +0200

    Implemented the `PhysicalVector<T>` to distinguish position and velocity

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
+use std::marker::PhantomData;
+
 #[derive(Debug)]
-struct PhysicalVector {
+struct PhysicalVector<T> {
     x: f64,
     y: f64,
     z: f64,
+    _t: PhantomData<T>,
 }
 
-impl PhysicalVector {
-    fn add(&mut self, other: &PhysicalVector) {
+impl<T> PhysicalVector<T> {
+    fn add(&mut self, other: &Self) {
         self.x = self.x + other.x;
         self.y = self.y + other.y;
         self.z = self.z + other.z;
     }
+    
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            x,
+            y,
+            z,
+            _t: PhantomData,
+        }
+    }
 }
 
 #[derive(Debug)]
-struct Particle {
-    position: PhysicalVector,
-    velocity: PhysicalVector,
-}
+struct Position;
+
+#[derive(Debug)]
+struct Velocity;
 
 fn main() {
-    let mut stone = Particle {
-        position: PhysicalVector {x: 0., y: 0., z: 0.},
-        velocity: PhysicalVector {x: 0., y: 0., z: 1.},
-    };
+    let mut pos = PhysicalVector::<Position>::new(0., 0., 0.);
+    //~ let mut vel = PhysicalVector::<Velocity> {x:1., y:0., z:0., _t:PhantomData};
     
-    dbg!(&stone);
+    dbg!(&pos);
     
-    stone.position.add(&PhysicalVector{x: 1., y: 0., z: 0.});
+    pos.add(&PhysicalVector::<Position>::new(0., 0., 1.));
+    pos.add(&PhysicalVector::<Velocity>::new(0., 0., 1.));
     
-    dbg!(&stone);
+    dbg!(&pos);
 }
