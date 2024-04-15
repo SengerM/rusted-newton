c574512ee346c5c64769fd82f67989392e456dd5
Matias Senger <m.senger@hotmail.com>
2024-04-15 15:48:44 +0200

    Moved all the code to a `particles_system.rs` file

---
+++ src/particles_system.rs
@@ -0,0 +1,214 @@
+use euclid::Vector3D;
+use sqlite;
+
+use super::geometric_objects;
+
+/// Defines units of position.
+pub enum PositionU {}
+/// Defines units of velocity.
+pub enum VelocityU {}
+/// Defines units of acceleration;
+pub enum AccelerationU {}
+pub enum ForceU {}
+/// Defines units of mass.
+pub type MassU = f64;
+
+/// Represents the concept of a particle in classical mechanics.
+pub struct Particle {
+    pub position: Vector3D::<f64, PositionU>,
+    pub velocity: Vector3D::<f64, VelocityU>,
+    pub mass: MassU,
+}
+
+type ParticleIdx = usize;
+
+/// Represents an interaction between two particles, which will lead to a force.
+pub enum Interaction {
+    force_between_two_particles(ParticleIdx,ParticleIdx,Force),
+    external_force(ParticleIdx,ExternalForce),
+}
+
+/// Represents a force.
+pub enum Force {
+    Elastic(f64, f64),
+    Damping(f64),
+    Gravitational,
+}
+
+impl Force {
+    /// Computes the force acting on `particle_1` due to this interaction.
+    fn acting_on_a(&self, a: &Particle, b: &Particle) -> Vector3D<f64, ForceU> {
+        let r = b.position-a.position;
+        match self {
+            Force::Elastic(k, d0) => (r.normalize()*(r.length() - (*d0))*(*k)).cast_unit(),
+            Force::Damping(c) => (r.normalize()*((b.velocity-a.velocity).dot(r.cast_unit())*(*c))).cast_unit(),
+            Force::Gravitational => (r.normalize()*a.mass*b.mass/r.square_length()).cast_unit(),
+        }
+    }
+    /// Computes the force acting on `particle_2` due to this interaction.
+    fn acting_on_b(&self, a: &Particle, b: &Particle) -> Vector3D<f64, ForceU> {
+        self.acting_on_a(a, b) * -1.
+    }
+}
+
+/// Represents an external force, i.e. a force that acts on a particle due to some external agent.
+pub enum ExternalForce {
+    LinearDrag(f64),
+}
+
+impl ExternalForce {
+    fn calculate_force(&self, a: &Particle) -> Vector3D<f64, ForceU> {
+        match self {
+            ExternalForce::LinearDrag(b) => (a.velocity*(*b)).cast_unit()*-1.,
+        }
+    }
+}
+
+pub enum Constraint {
+    external_constraint(ParticleIdx,ExternalConstraint),
+}
+
+pub enum ExternalConstraint {
+    infinite_wall(geometric_objects::Plane<PositionU>),
+    spherical_container(geometric_objects::Sphere<PositionU>),
+}
+
+impl ExternalConstraint {
+    fn compute_new_dynamical_variables(&self, particle: &Particle) -> (Vector3D<f64,PositionU>, Vector3D<f64,VelocityU>) {
+        match self {
+            ExternalConstraint::infinite_wall(wall) => {
+                let d = (particle.position - wall.position).dot(wall.normal);
+                if d < 0. {
+                    let new_vel: Vector3D<f64,VelocityU> = particle.velocity - (wall.normal.normalize()*2.0*(particle.velocity.dot(wall.normal.normalize().cast_unit()))).cast_unit();
+                    (particle.position, new_vel)
+                } else {
+                    (particle.position, particle.velocity)
+                }
+            }
+            ExternalConstraint::spherical_container(sphere) => {
+                if sphere.is_inside(&particle.position) {
+                    (particle.position, particle.velocity)
+                } else {
+                    let radial_direction = particle.position - sphere.center;
+                    let new_vel: Vector3D<f64,VelocityU> = particle.velocity - particle.velocity.project_onto_vector(radial_direction.cast_unit())*2.;
+                    (particle.position, new_vel)
+                }
+            }
+        }
+    }
+}
+
+/// Represents a system of particles, i.e. a collection of particles that interact.
+pub struct ParticlesSystem {
+    pub particles: Vec<Particle>,
+    pub interactions: Vec<Interaction>,
+    pub constraints: Vec<Constraint>,
+    time: f64,
+    n_time_saved_to_sql: usize,
+}
+
+impl ParticlesSystem {
+    /// Creates an empty particles system.  
+    pub fn new() -> Self {
+        Self {
+            particles: Vec::<Particle>::new(),
+            interactions: Vec::<Interaction>::new(),
+            constraints:  Vec::<Constraint>::new(),
+            time: 0.,
+            n_time_saved_to_sql: 0,
+        }
+    }
+    /// Add a particle to the system.
+    pub fn add_particle(&mut self, p: Particle) -> usize {
+        self.particles.push(p);
+        self.particles.len()
+    }
+    /// Add an interaction between two particles of the system.
+    pub fn add_interaction(&mut self, interaction: Interaction) {
+        self.interactions.push(interaction);
+    }
+    /// Add a constraint.
+    pub fn add_constraint(&mut self, constraint: Constraint) {
+        self.constraints.push(constraint);
+    }
+    /// Advance the time and update the system.
+    pub fn advance_time(&mut self, time_step: f64) {
+        // First we compute the acceleration of each particle using the interactions:
+        let mut accelerations = vec![Vector3D::<f64,AccelerationU>::zero(); self.particles.len()]; // A vector with one acceleration for each particle.
+        for interaction in &self.interactions {
+            match interaction {
+                Interaction::force_between_two_particles(idx_a, idx_b, force) => {
+                    let a = &self.particles[*idx_a];
+                    let b = &self.particles[*idx_b];
+                    accelerations[*idx_a] += force.acting_on_a(a,b).cast_unit()/a.mass;
+                    accelerations[*idx_b] += force.acting_on_b(a,b).cast_unit()/b.mass;
+                }
+                Interaction::external_force(idx,force) => {
+                    let a = &self.particles[*idx];
+                    accelerations[*idx] += force.calculate_force(a).cast_unit()/a.mass;
+                }
+            }
+        }
+        // Now we move the system forward in time:
+        for (n_particle,p) in self.particles.iter_mut().enumerate() {
+            let a = accelerations[n_particle];
+            let dv: Vector3D::<f64,VelocityU> = a.cast_unit()*time_step;
+            let dr: Vector3D::<f64,PositionU> = p.velocity.cast_unit()*time_step + dv.cast_unit()*time_step/2.;
+            p.position = p.position + dr;
+            p.velocity = p.velocity + dv;
+        }
+        // Now we check each constraint and make the required updates:
+        for constraint in &self.constraints {
+            match constraint {
+                Constraint::external_constraint(idx,constraint) => {
+                    let (new_pos, new_vel) = constraint.compute_new_dynamical_variables(&self.particles[*idx]);
+                    self.particles[*idx].position = new_pos;
+                    self.particles[*idx].velocity = new_vel;
+                }
+            }
+        }
+        self.time += time_step;
+	}
+    /// Creates an SQLite file to save the data.
+    pub fn create_sqlite_connection(&self, file_name: &String) -> sqlite::Connection {
+        let connection = sqlite::open(file_name).unwrap();
+        connection.execute("CREATE TABLE particles_system (n_time INTEGER, n_particle INTEGER, position_x FLOAT, position_y FLOAT, position_z FLOAT, velocity_x FLOAT, velocity_y FLOAT, velocity_z FLOAT, mass FLOAT);").unwrap();
+        connection.execute("CREATE TABLE time (n_time INTEGER, time FLOAT);").unwrap();
+        connection
+    }
+    /// Save the state of the system into an SQLite file.
+    pub fn dump_to_sqlite(&mut self, connection: &sqlite::Connection) {
+        for (n_particle,p) in self.particles.iter().enumerate() {
+            let mut query = String::new();
+            query.push_str("INSERT INTO particles_system VALUES (");
+            query.push_str(&self.n_time_saved_to_sql.to_string());
+            query.push_str(", ");
+            query.push_str(&n_particle.to_string());
+            query.push_str(", ");
+            query.push_str(&p.position.x.to_string());
+            query.push_str(", ");
+            query.push_str(&p.position.y.to_string());
+            query.push_str(", ");
+            query.push_str(&p.position.z.to_string());
+            query.push_str(", ");
+            query.push_str(&p.velocity.x.to_string());
+            query.push_str(", ");
+            query.push_str(&p.velocity.y.to_string());
+            query.push_str(", ");
+            query.push_str(&p.velocity.z.to_string());
+            query.push_str(", ");
+            query.push_str(&p.mass.to_string());
+            query.push_str(");");
+            connection.execute(query).unwrap();
+        }
+        let mut query = String::new();
+        query.push_str("INSERT INTO time VALUES (");
+        query.push_str(&self.n_time_saved_to_sql.to_string());
+        query.push_str(", ");
+        query.push_str(&self.time.to_string());
+        query.push_str(");");
+        connection.execute(query).unwrap();
+
+        self.n_time_saved_to_sql += 1;
+    }
+}
